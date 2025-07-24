ed74d17e5e6ba224709b894b03832fae6cb56a42
916446837@qq.com <916446837@qq.com>
2025-07-24 15:16:06 +0800

    优化UI

--- app/src/main/java/com/cht/smsforward/EmailConfigActivity.java
+++ app/src/main/java/com/cht/smsforward/EmailConfigActivity.java
@@ -10,7 +10,11 @@ import android.widget.EditText;
 import androidx.appcompat.widget.SwitchCompat;
 import android.widget.Toast;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 /**
  * Activity for configuring email settings
@@ -35,7 +39,14 @@ public class EmailConfigActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_email_config);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
         
         // Initialize managers
         settingsManager = new EmailSettingsManager(this);
